372244f80b7559aa0ec2bd6457f30cf41f712779
derks <patrick.deleo@icloud.com>
2025-09-23 01:58:38 -0400

    Commit done on 2025-09-23 01:59 by user "derks"

--- project/pd0/design/code/pd0.sv
+++ project/pd0/design/code/pd0.sv
@@ -35,5 +35,43 @@ module pd0 #(
   * probes. To be filled by student...
   *
   */
+logic [1:0]alu_sel;
+logic [DWIDTH-1:0] alu_op1;
+logic [DWIDTH-1:0] alu_op2;
+logic [DWIDTH-1:0] alu_res;
+logic alu_neg;
+logic alu_zero;
+
+alu #(.DWIDTH(DWIDTH)) alu_0 (
+    .sel_i (alu_sel),
+    .op1_i (alu_op1),
+    .op2_i (alu_op2),
+    .res_o (alu_res),
+    .zero_o (alu_zero),
+    .neg_o (alu_neg)
+);
+
+logic [DWIDTH-1:0] reg_rst_in;
+logic [DWIDTH-1:0] reg_rst_out;
+
+reg_rst #(.DWIDTH(DWIDTH)) reg_rst_0 (
+    .clk (clk),
+    .rst (reset),
+    .in_i (reg_rst_in),
+    .out_o (reg_rst_out)
+);
+
+logic [DWIDTH-1:0] three_stage_pipeline_op1;
+logic [DWIDTH-1:0] three_stage_pipeline_op2;
+logic [DWIDTH-1:0] three_stage_pipeline_res;
+
+three_stage_pipeline #(.DWIDTH(DWIDTH)) three_stage_pipeline_0 (
+    .clk (clk),
+    .rst (reset),
+    .op1_i (three_stage_pipeline_op1),
+    .op2_i (three_stage_pipeline_op2),
+    .res_o (three_stage_pipeline_res)
+);
+
 
 endmodule: pd0
