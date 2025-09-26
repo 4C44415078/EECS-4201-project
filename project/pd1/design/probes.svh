0f1356e3e47cc1480c158d86dbe8b640dcddb5f0
derks <patrick.deleo@icloud.com>
2025-09-26 10:35:56 -0400

    Commit completed on Friday, September 26, 2025 at 10:37 AM EST

--- project/pd1/design/probes.svh
+++ project/pd1/design/probes.svh
@@ -1,12 +1,13 @@
 // ----  Probes  ----
-//`define PROBE_ADDR      // ??
-//`define PROBE_DATA_IN   // ??
-//`define PROBE_DATA_OUT  // ??
-//`define PROBE_READ_EN   // ??
-//`define PROBE_WRITE_EN  // ??
+`define PROBE_ADDR memory_addr      
+`define PROBE_DATA_IN memory_data_i
+`define PROBE_DATA_OUT memory_data_o  
+`define PROBE_READ_EN memory_read_en
+`define PROBE_WRITE_EN memory_write_en
+`define PROBE_VALID   memory_valid_o
 
-//`define PROBE_F_PC // ??
-//`define PROBE_F_INSN // ??
+`define PROBE_F_PC fetch_pc_o
+`define PROBE_F_INSN fetch_insn_o
 // ----  Probes  ----
 
 // ----  Top module  ----
