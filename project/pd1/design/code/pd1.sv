e99171c1bdf544cf6e574cfb2de3971dec50c694
derks <patrick.deleo@icloud.com>
2025-09-26 10:35:56 -0400

    Commit completed on Friday, September 26, 2025 at 10:37 AM EST

--- project/pd1/design/code/pd1.sv
+++ project/pd1/design/code/pd1.sv
@@ -32,18 +32,4 @@ module pd1 #(
         .insn_o(insn_o)
     );
 
-    memory #(
-        .AWIDTH(AWIDTH),
-        .DWIDTH(DWIDTH),
-        .BASE_ADDR(32'h01000000)
-    ) memory_pd1 (
-        .clk(clk),
-        .rst(rst),
-        .addr_i(##SOMETHING),
-        .data_i(DWIDTH'd0),
-        .read_en_i(1'b1),
-        .write_en_i(1'b0),
-        .data_o(##SOMETHING)
-    );
-
 endmodule : pd1
