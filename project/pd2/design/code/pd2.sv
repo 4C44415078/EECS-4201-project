16ddc619cb2b1b5f19b215a549a2e627be691ea9
derks <patrick.deleo@icloud.com>
2025-10-10 18:52:04 -0400

    Commit done on 2025-10-10 @6:52 PM EST.

--- project/pd2/design/code/pd2.sv
+++ project/pd2/design/code/pd2.sv
@@ -21,4 +21,118 @@ module pd2 #(
   *
   */
 
+  // imemory signals
+    logic [DWIDTH - 1:0] addr_i;
+    logic [DWIDTH - 1:0] data_i;
+    logic write_en;
+    logic read_en;
+       
+  // Fetch signals
+    logic [DWIDTH - 1:0] f_pc;
+    logic [DWIDTH - 1:0] f_insn;
+       
+    memory #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASE_ADDR(32'h01000000)
+       ) memory1 (
+        .clk(clk),
+        .rst(reset),
+        .addr_i(f_pc),
+        .data_i(data_i),
+        .read_en_i(read_en),
+        .write_en_i(write_en),
+        .data_o(f_insn)
+   );
+ 
+    assign read_en = 1'b1;
+    assign write_en = 1'b0;
+
+  // Fetch
+    fetch #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASEADDR(32'h01000000)
+    ) fetch1 (
+        .clk(clk),
+        .rst(reset),
+        .pc_o(f_pc),            
+        .insn_o(f_insn)         
+    );
+
+    // Decode signals
+    logic [DWIDTH - 1:0] d_pc;
+    logic [DWIDTH - 1:0] d_insn;
+    logic [6:0] d_opcode;
+    logic [4:0] d_rd;
+    logic [4:0] d_rs1;
+    logic [4:0] d_rs2;
+    logic [6:0] d_funct7;
+    logic [2:0] d_funct3;
+    logic [4:0] d_shamt;
+    logic [DWIDTH - 1:0] d_imm;
+
+    decode #(
+        .AWIDTH(32),
+        .DWIDTH(32)
+    ) decode1 (
+        .clk(clk),
+        .rst(reset),
+        .insn_i(f_insn),
+        .pc_i(f_pc),
+        .pc_o(d_pc),            
+        .insn_o(d_insn),         
+        .opcode_o(d_opcode),         
+        .rd_o(d_rd),         
+        .rs1_o(d_rs1),         
+        .rs2_o(d_rs2),         
+        .funct7_o(d_funct7),         
+        .funct3_o(d_funct3),         
+        .shamt_o(d_shamt),         
+        .imm_o(d_imm)          
+    );
+
+    // Immediate Generator signals
+    logic [DWIDTH - 1:0] igen_imm;
+    // Immediate Generator instantiation
+    igen #(
+        .DWIDTH(32)
+    ) igen1 (
+        .opcode_i(d_opcode),
+        .insn_i(d_insn),
+        .imm_o(igen_imm)
+    );
+
+    // Control signals
+    logic c_pcsel;
+    logic c_immsel;
+    logic c_regwren;
+    logic c_rs1sel;
+    logic c_rs2sel;
+    logic c_memren;
+    logic c_memwren;
+    logic [1:0] c_wbsel;
+    logic [3:0] c_alusel;
+
+    control #(
+        .DWIDTH(32)
+    ) control1 (
+        .insn_i(d_insn),
+        .opcode_i(d_opcode),
+        .funct7_i(d_funct7),
+        .funct3_i(d_funct3),
+        .pcsel_o(c_pcsel),       
+        .immsel_o(c_immsel),
+        .regwren_o(c_regwren),
+        .rs1sel_o(c_rs1sel),
+        .rs2sel_o(c_rs2sel),
+        .memren_o(c_memren),
+        .memwren_o(c_memwren),
+        .wbsel_o(c_wbsel),
+        .alusel_o(c_alusel)
+    );
+
+    assign d_imm = igen_imm;
+    assign d_shamt = igen_imm[4:0];
+
 endmodule : pd2
