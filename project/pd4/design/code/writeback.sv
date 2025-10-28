c44e74f06c21de735f2385549ac526f27b3f598e
derks <patrick.deleo@outlook.com>
2025-10-28 10:56:44 -0400

    Commit 10-28-2025 @ 10:57 EST. Started on PD4.

--- project/pd4/design/code/writeback.sv
+++ project/pd4/design/code/writeback.sv
@@ -24,6 +24,11 @@
      input logic [DWIDTH-1:0] memory_data_i,
      input logic [1:0] wbsel_i,
      input logic brtaken_i,
+     // ADDED SIGNALS --------------------------
+     input logic [3:0] wbsel_i,
+     input logic [DWIDTH-1:0] imm_i,
+     input logic pcsel_i,
+     // ----------------------------------------
      output logic [DWIDTH-1:0] writeback_data_o,
      output logic [AWIDTH-1:0] next_pc_o
  );
@@ -32,5 +37,14 @@
      * Process definitions to be filled by
      * student below...
      */
+    
+    always_comb begin
+        writeback_data_o = (wbsel_i == `WB_ALU) ? alu_res_i :
+            (wbsel_i == `WB_MEM) ? memory_data_i :
+            (wbsel_i == `WB_PC4) ? pc_i :
+            (wbsel_i == `WB_IMM) ? imm_i : {DWIDTH{1'b0}};
+        
+        next_pc_o = (pcsel_i) ? alu_res_i : pc_i + 32'd4;
+    end
 
 endmodule : writeback
